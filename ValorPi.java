4930cedc08e03c1bba6b024db21f09054ec26578
willjpg <willianf.silva@outlook.com>
2024-03-07 22:33:50 -0300

    ValorPi

--- ValorPi.java
+++ ValorPi.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.mavenproject1.newpackage;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Willi
  */
 public class ValorPi {
-    
+
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        int i = 0;
+        double pi = 0;
+
+        int b = leia.nextInt();
+        //int c = leia.nextInt();
+
+        for (i = 0; i <= b; i++) {
+
+            pi = (pi + 4 * (Math.pow((-1), i) / (2 * i + 1)));
+
+        }
+        System.out.println(pi);
+    }
+
+        public static double calcularPi(int n) {
+        int i = 0;
+        double pi = 0;
+        
+        for (i = 0; i <= n; i++) {
+
+            pi = (pi + 4 * (Math.pow((-1), i) / (2 * i + 1)));
+            
+        }
+        return pi;
+}
 }
